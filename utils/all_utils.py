041f98d6ffde6551f145a2b0a5efe241191ea3de
sarveshk4 <sarvesh4347@gmail.com>
2021-09-21 21:55:47 +0530

    docstring updated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -2,11 +2,25 @@ import os
 import joblib
 
 def prepare_data(df):
+    """It is used to separate the dependent variable and independent variable
+
+    Args:
+        df (pd.DataFrame): its the pandas DataFrame to
+
+    Returns:
+        tuple: it return the tuple of dependent and independent variable
+    """
     X = df.drop("y",axis=1)
     y=df["y"]
     return X,y
 
 def save_model(model,filename):
+    """This saves the trained model 
+
+    Args:
+        model (python object): trained model to
+        filename (str): path to save the trained model
+    """
     model_dirs = "models"
     os.makedirs(model_dirs,exist_ok=True)
     filePath = os.path.join(model_dirs,filename)
