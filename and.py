2df81550411bc4e077acbd0bb695bd5bc536368a
sarveshk4 <sarvesh4347@gmail.com>
2021-09-21 22:53:59 +0530

    docstring updated

--- and.py
+++ and.py
@@ -1,24 +1,34 @@
-import pandas as pd
-
 from utils.model import Perceptron
-from utils.all_utils import prepare_data,save_model
+from utils.all_utils import prepare_data, save_model
+import pandas as pd
+import logging
+import os
 
-AND = {
-    "x1":[0,0,1,1],
-    "x2":[0,1,0,1],
-    "y":[0,0,0,1]
-}
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_log.log"),level=logging.INFO, format=logging_str,filemode="a")
 
-df= pd.DataFrame(AND)
 
-X,y = prepare_data(df)
+def main(data, modelName, eta, epochs):
+    df = pd.DataFrame(data)
+    logging.info(f"This is actual dataFrame {df}")
+    X, y = prepare_data(df)
+    model = Perceptron(learn_rate=eta, epochs=epochs)
+    model.fit(X, y)
+    _ = model.total_loss()
+    save_model(model, filename=modelName)
 
-LEARN_RATE = 0.3
-EPOCHS = 10
-
-model = Perceptron(learn_rate=LEARN_RATE,epochs=EPOCHS)
-model.fit(X,y)
-
-_ = model.total_loss()
-
-save_model(model,filename="and_model")
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    try:
+        main(data=AND, modelName="and_model", eta=ETA, epochs=EPOCHS)
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/model.py
+++ utils/model.py
@@ -1,9 +1,11 @@
 import numpy as np
+import logging
+from tqdm import tqdm
 
 class Perceptron:
     def __init__(self,learn_rate,epochs):
         self.weights = np.random.randn(3)*1e-4
-        print(f"initial weights before training: {self.weights}")
+        logging.info(f"initial weights before training: {self.weights}")
         self.learn_rate = learn_rate
         self.epochs = epochs
     
@@ -16,20 +18,20 @@ class Perceptron:
         self.y = y
         
         X_with_bias = np.c_[self.X,-np.ones((len(self.X),1))]
-        print(f"X with bias: {X_with_bias}")
+        logging.info(f"X with bias: {X_with_bias}")
         
-        for epoch in range(self.epochs):
-            print("--"*10)
-            print(f"for epoch : \n{epoch}")
-            print("--"*10)
+        for epoch in tqdm(range(self.epochs), total=self.epochs, desc='training the model'):
+            logging.info("--"*10)
+            logging.info(f"for epoch : \n{epoch}")
+            logging.info("--"*10)
         
             y_hat = self.activationFunction(X_with_bias,self.weights)
-            print(f"predicted value after forward pass : \n{y_hat}")
+            logging.info(f"predicted value after forward pass : \n{y_hat}")
             self.error = self.y - y_hat
-            print(f"error : {self.error}")
+            logging.info(f"error : {self.error}")
             self.weights = self.weights + self.learn_rate*np.dot(X_with_bias.T,self.error)
-            print(f"updated weights after epoch  : {epoch}/{self.epochs}:{self.weights}")
-            print("#####"*10)
+            logging.info(f"updated weights after epoch  : {epoch}/{self.epochs}:{self.weights}")
+            logging.info("#####"*10)
     
     def predict(self,X):
         X_with_bias = np.c_[X,-np.ones((len(X),1))]
@@ -37,5 +39,5 @@ class Perceptron:
     
     def total_loss(self):
         total_loss = np.sum(self.error)
-        print(f"total loss : {total_loss}")
+        logging.info(f"total loss : {total_loss}")
         return total_loss
